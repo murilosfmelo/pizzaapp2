5423c0445e29bb3bf12c5ab99fccf3788a0155b1
MURILO SABINO FARIAS DE MELO <murilo.sfmelo@senacsp.edu.br>
2023-07-21 14:46:52 -0300

    tamanhos e produtos funcionando

---
+++ resources/views/produto/index.blade.php
@@ -0,0 +1,50 @@
+@extends('layouts.base')
+@section('content')
+
+    <h1>Produtos</h1>
+    <table class="table table-striped table-hover">
+        <thead>
+            <tr>
+                <th class="col-2">Ações</th>
+                <th class="col-1">ID</th>
+                <th>Produto</th>
+                <th>Observações</th>
+                <th>Qtd Tamanhos</th>
+
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($produtos->get() as $produto)
+            <tr>
+                <td>
+                    <a class="btn btn-primary" href="{{ route('produto.edit', ['id'=>$produto->id_produto]) }}">
+                        <i class="bi bi-pen"></i>
+                    </a>
+                    <a class="btn btn-success" href="{{ route('produto.show', ['id'=>$produto->id_produto]) }}">
+                        <i class="bi bi-eyeglasses"></i>
+                    </a>
+                    <a class="btn btn-danger" href="{{ route('produto.destroy', ['id'=>$produto->id_produto]) }}">
+                        <i class="bi bi-recycle"></i>
+                    </a>
+                </td>
+                <td>
+                    {{ $produto->id_produto}}
+                </td>
+                <td>
+                    {{ $produto->nome }}
+                </td>
+                <td>{{ nl2br($produto->observacoes) }}</td>
+                <td>
+                    {!!
+                        $produto->tamanhos()->count()
+                    !!}
+                </td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+
+@endsection
+@section('scripts')
+
+@endsection
---
+++ resources/views/produto/show.blade.php
@@ -0,0 +1,61 @@
+@extends('layouts.base')
+@section('content')
+
+<h1> Produto:{{ $produto->nome }} </h1>
+<h2> Tipo: {{ $produto->tipo->tipo}}</h2>
+<p>Descrição: {!!nl2br($produto->descricao)!!}</p>
+
+
+@if ($produto->observacoes)
+
+    <p class="alert alert-info">
+        {!!!nl2br($produto->observacoes)!!}
+    </p>
+
+@endif
+
+<table class="table table-striped table-hover">
+    <thead>
+        <tr>
+            <th class="col-2">Ações</th>
+            <th>Tamanho</th>
+            <th>Preço</th>
+            <th>Obsevções</th>
+        </tr>
+    </thead>
+    <tbody>
+        @forelse ($produto->tamanhos()->get() as $item )
+                                                {{-- $item = $tamanho --}}
+
+         <tr>
+            <td>
+                {{-- editar --}}
+                <a class="btn btn-primary" href="#">
+                    <i class="fa-solid fa-pen-to-square"></i>
+                </a>
+            </td>
+            <td>
+                {!!$item->tamanho->tamanho!!}
+
+            </td>
+            <td>
+                {{$item->preco}}
+            </td>
+            <td>
+                {!! nl2br($item->observacoes)!!}
+            </td>
+        </tr>
+        @empty
+        <tr>
+            <td colspan="4">
+                Nenhum tamanho definido para esse produto
+            </td>
+        </tr>
+        @endforelse
+    </tbody>
+</table>
+
+@endsection
+@section('scripts')
+
+@endsection
